cb9da3490f931e8a2ca4e7e3207443fe0d8626b3
Kaiquii <112667885+Kaiquii@users.noreply.github.com>
2022-09-13 20:27:33 -0300

    Add files via upload

---
+++ Calculadora_3.0_20220527_100622.py
@@ -0,0 +1,41 @@
+import math
+while True: 
+ Operação = input("Qual operação (+,-,*,/,raiz,sen,cos,tan) você quer fazer? ou se deseja fechar a calculadora digite (sair) ")
+ if Operação == "+":
+     numero1 = input("Digite um valor:")
+     numero2 = input("Digite um valor:")
+     Resultado = int(numero1) + int(numero2)
+     print ("O resultado da operação é: " + str(Resultado)) 
+ if   Operação == "-":
+     numero1 = input("Digite um valor:")
+     numero2 = input("Digite um valor:")
+     Resultado = int(numero1) - int(numero2)
+     print ("O resultado da operação é: " + str(Resultado))
+ if Operação == "*":
+     numero1 = input("Digite um valor:")
+     numero2 = input("Digite um valor:")
+     Resultado = int(numero1) * int(numero2)
+     print ("O resultado da operação é: " + str(Resultado))
+ if Operação == "/":
+     numero1 = input("Digite um valor:")
+     numero2 = input("Digite um valor:")
+     Resultado = int(numero1) / int(numero2)
+     print ("O resultado da operação é: " + str(Resultado))
+ if Operação == "raiz":
+     numero = int(input("Digite um valor:"))
+     raiz = math.sqrt(numero)
+     print ("O resultado da operação é: " , (raiz))
+ if Operação == "sen":
+     ângulo = float(input("Digite o ângulo que você deseja: "))
+     seno = math.sin(math.radians(ângulo))
+     print("O ângulo de {} tem o seno de {:.2f}". format(ângulo, seno))
+ if Operação == "cos":
+     ângulo = float(input("Digite o ângulo que você deseja: "))
+     cosseno = math.cos(math.radians(ângulo))
+     print("O ângulo de {} tem o cosseno de {:.2f}". format(ângulo, cosseno))
+ if Operação == "tan":
+     ângulo = float(input("Digite o ângulo que você deseja: "))
+     tangente = math.tan(math.radians(ângulo))
+     print("O ângulo de {} tem o tangente de {:.2f}". format(ângulo, tangente))
+ if Operação == "sair":
+     break
